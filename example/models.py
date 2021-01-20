106c01a379ba86da87ded6f0fb0648719fa59460
unknown <Python@BUE302D14.alunos.sc.senac.br>
2021-01-20 16:49:26 -0300

    alterações

--- example/models.py
+++ example/models.py
@@ -20,3 +20,26 @@ class Album(models.Model):
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_stars = models.IntegerField()
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    publications = models.ManyToManyField(Publication)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = ['headline']
