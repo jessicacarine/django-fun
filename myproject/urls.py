c1e845b89d5b14da57a693513481ac8ba8d2a1fa
unknown <Python@BUE302D14.alunos.sc.senac.br>
2021-01-21 17:18:54 -0300

    atualização accounts

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
+from accounts import views as accounts_views
 
 urlpatterns = [
+    url(r'^signup/$', accounts_views.signup, name='signup'),
     path('admin/', admin.site.urls),
     path('pokemaoc/', include('pokemaoc.urls'))
 ]
